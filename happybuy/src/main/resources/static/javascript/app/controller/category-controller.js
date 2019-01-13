d1659eb4ad00a9f2a940fdcc5e61c45c09815fd0
LAPTOP-74J1HNPP\sofia <sofia.tuyet@gmail.com>
2019-01-13 09:04:15 -0800

    completed pagination includes showing products for each page clicked

--- happybuy/src/main/resources/static/javascript/app/controller/category-controller.js
+++ happybuy/src/main/resources/static/javascript/app/controller/category-controller.js
@@ -1,15 +1,19 @@
 happybuy.controller('CategoryProductCtrl', ['$scope','$http','$window','$rootScope', 'AuthService', '$routeParams',
 function($scope, $http, $window, $rootScope, authService, $routeParams){
-
+	
+	$scope.maxSize = 0;
 	$scope.categoryId = $routeParams.categoryId;  
-	$scope.getProdByCate = function(){
+	$scope.pageList=[];
+	$scope.getProdByCate = function(pageNumber){
+		console.log("pageNumber: ", pageNumber);		
 		var request = {        
 			"data": $scope.categoryId,
 			"metadata": {
-				"pageNumber": 1,
+				"pageNumber": pageNumber,
 				"pageSize": 15              
 				}            
-			}				
+			}	
+			console.log("request: ", request);			
 		$http({
 			url: "/no-auth/product/find-by-category",
 			method: "POST",
@@ -24,7 +28,7 @@ function($scope, $http, $window, $rootScope, authService, $routeParams){
 			alert("Server is error, please try again!")
 		});	
 	}
-	$scope.getProdByCate();
+	$scope.getProdByCate(1);
 
 	$scope.getNumberOfPages = function () {
 		$http({
@@ -32,20 +36,49 @@ function($scope, $http, $window, $rootScope, authService, $routeParams){
 			method: "GET",
 			params: {
 				categoryId: $scope.categoryId,
-				itemPerPage: 5
+				itemPerPage: 15
 			}
 		})
-		.then(function(res){		
-			$scope.pageList = $scope.getNumber(res.data);
-			console.log($scope.pageList);
+		.then(function(res){
+			var totalPage = res.data;			
+			$scope.maxSize = res.data;
+			if (totalPage > 5){
+				totalPage = 5;
+			}		
+			for (var i = 1; i<=totalPage; i++){
+				$scope.pageList.push(i);
+			}			
+			console.log("getNumOfPage:", $scope.pageList);				
 		})
 		.catch(function(response){
 			alert("Server is error, please try again")
 		});
 	}
-	$scope.getNumberOfPages();
-
-	$scope.getNumber = function(num) {
-		return new Array(num);
-	}
+	$scope.getNumberOfPages();	
+	
+	$scope.clickPage = function(index){			
+		$scope.pageList = [];	
+		var begin = index - 2;
+		var end = index + 2;
+		console.log("maxSize: ", $scope.maxSize);	
+		if ($scope.maxSize > 5){
+			if (begin <= 0){
+				begin = 1;
+				end = 5;
+			}
+			if (end >= $scope.maxSize){
+				begin = $scope.maxSize - 4;
+				end = $scope.maxSize;				
+			}			
+		}else{
+			begin = 1;
+			end = $scope.maxSize;		
+		}			
+		for (var i=begin; i<=end; i++){
+			$scope.pageList.push(i);								
+		}	
+		$scope.getProdByCate(index);		
+		console.log("clickPage: ", $scope.pageList);
+	}		
+	
 }]);
